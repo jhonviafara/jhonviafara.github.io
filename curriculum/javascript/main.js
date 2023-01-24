f9812549ee31d96ace49d25608e4fc6c5a061b0b
jhon alejandro viafara <jhon1144105@gmail.com>
2023-01-24 01:44:42 -0300

    version del 24/01/23

--- curriculum/javascript/main.js
+++ curriculum/javascript/main.js
@@ -1,15 +1,3 @@
 function downloadDoc() {
-   var doc = new jspdf('p', 'pt','letter');
-   var margin = 10;
-   var scale =(doc.internal.pageSize.width - margin * 2) / document.body.scrollWidth;
-   doc.html(document.body, {
-        x: margin,
-        y: margin,
-        html2canvas:{
-            scale: scale,
-        },
-        callback: function (doc){
-            doc.ouput( 'dataurlnewwindow' , {filename : 'fichero-pdf.pdf'});
-        }
-   });   
+    window.print();
 }
