26cf9899b77cf7d947a41e285c450ff534cc7a39
jhon alejandro viafara <jhon1144105@gmail.com>
2023-02-15 17:33:08 -0300

    version del dia 15/2/2023

--- villa_albino/scripts/main.js
+++ villa_albino/scripts/main.js
@@ -9,16 +9,3 @@ hamburgerButton.addEventListener('click', () => {
   
   menu.classList.toggle('active');
 });
-function cambiarImagenes() {
-    var imagenes = document.getElementById("portada").getElementsByTagName("img");
-    var numImagenes = imagenes.length;
-    var i = 0;
-  
-    setInterval(function() {
-      imagenes[i].style.display = "flex";
-      i = (i + 1) % numImagenes;
-      imagenes[i].style.display = "flex";
-    }, 3000);
-  }
-  
-  cambiarImagenes();
